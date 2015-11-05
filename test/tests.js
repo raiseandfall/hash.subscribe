728853bfac3ca476a49525a1221165efbbb945ea
raiseandfall <druidev+github@gmail.com>
2015-11-04 16:54:34 -0800

    [TEST] Added new test

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,29 @@
 'use strict'
 
 var tape = require('tape');
-var Hash = require('../');
+var Hash = require('../hash');
+console.log('t:', Hash);
 
 tape('Hash.subscribe is defined', function(t) {
   t.plan(1);
   t.notEqual(Hash, null);
 });
 
-tape('Hash.subscribre is a singleton', function(t) {
+tape('Hash.subscribe is a singleton', function(t) {
   t.plan(1);
   var hashInstance = Hash;
-  t.equal(hashInstance, Hash);
+  t.deepEqual(hashInstance, Hash);
 });
 
+tape('Hash can be initialized', function(t) {
+  t.plan(1);
+  var initHash = 'foo=bar';
+
+console.log('t:',Hash);
+
+  //Hash.init(initHash);
+
+  console.log(window.location.hash);
+
+  t.equal(window.location.hash, initHash);
+});
